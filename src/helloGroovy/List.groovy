d00c4ee7fa4872da99cbe8e42e42aa142577e419
guokun.hou <hgk324@163.com>
2019-03-12 08:05:57 +0800

    first commit

---
+++ src/helloGroovy/List.groovy
@@ -0,0 +1,15 @@
+package helloGroovy
+
+def lists=['groovy','gradle','android']
+
+println lists.size
+
+lists.each{
+	list->println list
+}
+
+
+assert lists[0]=='groovy'
+lists[100]=100
+println lists.size
+println lists[99]
